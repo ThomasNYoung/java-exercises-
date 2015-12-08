38d05f78377e30e544846be2d6e7308fb2e5354d
Tom Young <tom.young.satx@gmail.com>
2015-12-08 15:43:02 -0600

    Switch Statements

---
+++ BlockDemo.java
@@ -0,0 +1,11 @@
+class BlockDemo{
+	public static void main(String[] args){
+		boolean condition = false;
+		if(condition) { // begin block 1
+			System.out.println("condition is true.");
+		} //end block 1
+		else{ //begin block 2
+			System.out.println("condition is false.");
+		}
+	}
+}
