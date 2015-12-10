666f85ac1a7675e03a27b947ca249b33b0fa816c
Tom Young <tom.young.satx@gmail.com>
2015-12-10 14:53:16 -0600

    Branching Statements

---
+++ BreakWithLabelDemo.java
@@ -0,0 +1,29 @@
+class BreakWithLabelDemo{
+	public static void main(String[] args){
+		int[][] arrayOfInts = {
+			{32,87,3,589},
+			{12,1076,2000,8},
+			{622,127,77,955}
+		};
+		int searchfor = 70;
+
+		int i;
+		int j = 0;
+		boolean foundIt = false;
+
+		search:
+			for(i=0; i<arrayOfInts.length; i++){
+				for (j=0; j<arrayOfInts[i].length ; j++) {
+					if (arrayOfInts[i][j] == searchfor) {
+						foundIt = true;
+						break search;
+					}
+				}
+			}
+			if (foundIt) {
+				System.out.println("found " + searchfor + " at " + i + ", " + j);
+			}else{
+				System.out.println(searchfor + " not in the array");
+			}
+	}
+}
